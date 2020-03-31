cfbda2203906319439dad70d2b1b72bd48cfc9ba
Luana Nunes <luana.dacostanunes@gmail.com>
2020-03-31 14:59:58 -0300

    controle de posicao

---
+++ ControleDePosicao_PID.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 14:59:12 2020
+
+@author: Luana Nunes
+"""
+
+import numpy as np
+import control as co_general
+import matplotlib.pyplot as plt
+import control.matlab as co
+
